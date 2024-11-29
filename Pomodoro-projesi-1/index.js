2ef8c00fde8a07d16a5a74adc6dfbb7b28402afb
Çınar Özgürgil <cozgurgil@gmail.com>
2024-11-29 20:08:21 +0300

    son güncellemeler bitiyor

--- Pomodoro-projesi-1/index.js
+++ Pomodoro-projesi-1/index.js
@@ -1,8 +1,11 @@
 const Minus = document.querySelector("#minus");
 const BtnText = document.querySelector("#btnText");
 const Plus = document.querySelector("#plus");
+let intervalId;
+let timerstarted = false;
 
 document.querySelector("#plus").addEventListener("click", function () {});
+
 
 document.querySelector("#btnStart").addEventListener("click", function () {
 
@@ -12,24 +15,49 @@ document.querySelector("#btnStart").addEventListener("click", function () {
       if (BtnText.value == "" ||  (zero == 0 && second == 0)) {
         alert("Lütfen çalışacağınız dersi giriniz eğer süre girmediyseniz lütfen süre belirtiniz.");
 
-      } else if(!(zero == 0 && second == 0)){
+      } else if(!(zero == 0 && second == 0) && !timerstarted){
         TimerStart();
-      } 
+        document.querySelector("#btnStop").style.display = "inline-block";
+        timerstarted = true;
+      } else if(intervalId) {
+        timerstarted = false;
+      }
       else {
         document.querySelector("#warning-text").style.display = "none";
+        document.querySelector("#plus").style.display = "none";
+        document.querySelector("#minus").style.display = "none";
         startTime();
-  }
-      if(zero == 0 && second == 0) {
-        addLesson();
-        addTime();
       }
+
+      
+});
+
+document.querySelector("#btnReplay").addEventListener("click", () => {
+
+  if(!(zero == 0 && second == 0)) {
+    let html = `
+        <span>${localStorage.getItem("zero") < 10 ? "0" + zero : localStorage.getItem("zero")}</span>:<span>${localStorage.getItem("second") < 10 ? "0" + second : localStorage.getItem("second")}</span>
+    `;
+    document.querySelector("#sayaç").innerHTML = html;
+    console.log(html)
+    TimerStart();
+  }
+
+
 });
 
 document.querySelector("#buraya-ekle").addEventListener("click", () => {
-     TimerStart();
+  if(!(zero == 0 && second == 0) && !timerstarted){
+    TimerStart();
+    timerstarted = true;
+  }
 });
 
-document.querySelector("#btnStop").addEventListener("click", function () {
+document.querySelector("#btnStop").addEventListener("click", function () { 
+  
+  clearInterval(intervalId);
+  console.log("Zamanlıyıcı Durduruldu");
+
   let html = `
         <button type="button" class="btn btn-success mt-2" id="button-finish">Bitir</button>
     `;
@@ -41,9 +69,15 @@ document.querySelector("#btnStop").addEventListener("click", function () {
 });
 
 document.querySelector("#buraya-ekle").addEventListener("click", () => {
-  document.querySelector("#buraya-ekle").innerHTML = "";
+  if(!(zero == 0 && second == 0)) {
+    document.querySelector("#buraya-ekle").innerHTML = "";
     addLesson();
     addTime();
+    document.querySelector("#plus").style.display = "inline-block";
+    document.querySelector("#minus").style.display = "inline-block";
+  }
+ 
+
 });
 
 function zeroTime() {
@@ -79,7 +113,8 @@ function startTime() {
 }
 
 function TimerStart() {
-    let intervalId = setInterval(function() {
+
+    intervalId = setInterval(function() {
         second--
 
         if(second == -1) {
@@ -88,17 +123,15 @@ function TimerStart() {
         } else if(zero == 0 && second == 0) {
             clearInterval(intervalId);
             console.log("Interval Durduruldu")
+            addLesson();
+            addTime();
         }
 
-        console.log(second);
-        console.log(zero);
         startTime();
-    }, 1000);
+    }, 10);
 }
 
-function TimeStop() {
-  
-}
+
 
 function addLesson() {
     let html = `
@@ -108,8 +141,8 @@ function addLesson() {
 }
 
 function addTime() {
-  let html = `
+  let htmlls = `
       ${localStorage.getItem("zero") < 10 ? "0" + localStorage.getItem("zero") : localStorage.getItem("zero")}:${localStorage.getItem("second") < 10 ? "0" + localStorage.getItem("second") : localStorage.getItem("second")}
   `
-  document.querySelector("#time").insertAdjacentHTML("afterbegin", html);
+  document.querySelector("#time").insertAdjacentHTML("afterbegin", htmlls);
 }
