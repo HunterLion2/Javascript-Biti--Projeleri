d701f6774c3cf79a465eb556b0875604caf26cc2
Çınar Özgürgil <cozgurgil@gmail.com>
2024-12-01 15:11:40 +0300

    Son

--- Pomodoro-projesi-1/index.js
+++ Pomodoro-projesi-1/index.js
@@ -7,6 +7,7 @@ let timerstarted = false;
 document.querySelector("#plus").addEventListener("click", function () {});
 
 
+// Başlat Butonu
 document.querySelector("#btnStart").addEventListener("click", function () {
 
       localStorage.setItem("zero", zero);
@@ -32,6 +33,7 @@ document.querySelector("#btnStart").addEventListener("click", function () {
       
 });
 
+// Tekrarlama Butonu
 document.querySelector("#btnReplay").addEventListener("click", () => {
 
   if(!(zero == 0 && second == 0)) {
@@ -46,38 +48,68 @@ document.querySelector("#btnReplay").addEventListener("click", () => {
 
 });
 
-document.querySelector("#buraya-ekle").addEventListener("click", () => {
+// Bitir Butonu
+document.querySelector("#Bitir").addEventListener("click", () => {
   if(!(zero == 0 && second == 0) && !timerstarted){
     TimerStart();
     timerstarted = true;
   }
 });
 
+// Durdurma Butonu
 document.querySelector("#btnStop").addEventListener("click", function () { 
   
   clearInterval(intervalId);
   console.log("Zamanlıyıcı Durduruldu");
 
   let html = `
-        <button type="button" class="btn btn-success mt-2" id="button-finish">Bitir</button>
+        Bitir
     `;
 
   localStorage.setItem("zero2" , zero);
   localStorage.setItem("second2" , second);
   
-  document.querySelector("#buraya-ekle").innerHTML = html;
+  document.querySelector("#Bitir").innerHTML = html;
+
+  let httml = `
+        Devam Et
+  `
+
+  document.querySelector("#Continue").innerHTML = httml;
+
+
+
+  document.querySelector("#btnReplay").style.display = "none";
+  document.querySelector("#btnStop").style.display = "none";
+  document.querySelector("#btnStart").style.display = "none";
+
+  document.querySelector("#Bitir").style.display = "inline-block";
+  document.querySelector("#Continue").style.display = "inline-block";
+
 });
 
-document.querySelector("#buraya-ekle").addEventListener("click", () => {
+// Bitir Butonu
+document.querySelector("#Bitir").addEventListener("click", () => {
   if(!(zero == 0 && second == 0)) {
-    document.querySelector("#buraya-ekle").innerHTML = "";
+    localStorage.setItem("zeros", zero);
+    localStorage.setItem("seconds", second)
+    
+    dakika = localStorage.getItem("zero") - localStorage.getItem("zeros");
+    saniye = localStorage.getItem("seconds") - localStorage.getItem("second");
+
+    document.querySelector("#Bitir").style.display = "none";
+    document.querySelector("#Continue").style.display = "none";
+
     addLesson();
-    addTime();
+    addTimeStop(dakika, saniye);
+    startTurn();
+
     document.querySelector("#plus").style.display = "inline-block";
     document.querySelector("#minus").style.display = "inline-block";
+    document.querySelector("#btnStart").style.display = "inline-block";
+    document.querySelector("#btnStop").style.display = "inline-block";
+    document.querySelector("#btnReplay").style.display = "inline-block";
   }
- 
-
 });
 
 function zeroTime() {
@@ -145,4 +177,29 @@ function addTime() {
       ${localStorage.getItem("zero") < 10 ? "0" + localStorage.getItem("zero") : localStorage.getItem("zero")}:${localStorage.getItem("second") < 10 ? "0" + localStorage.getItem("second") : localStorage.getItem("second")}
   `
   document.querySelector("#time").insertAdjacentHTML("afterbegin", htmlls);
+}
+
+function addTimeStop(dakika, saniye) {
+
+  let Saniyeler = 60 - saniye;
+  let dakikalar = dakika - 1;
+
+  let htmlls = `
+      ${dakikalar < 10 ? "0" + dakikalar : dakikalar}:${Saniyeler < 10 ? "0" + Saniyeler : Saniyeler}
+  `
+  document.querySelector("#time").insertAdjacentHTML("afterbegin", htmlls);
+
+  
+
+}
+
+function startTurn() {
+    zero = 0;
+    second = 0;
+
+    let html = `
+        <span>${zero < 10 ? "0" + zero : zero}</span>:<span>${second < 10 ? "0" + second : second}</span>
+    `;
+    document.querySelector("#sayaç").innerHTML = html;
+
 }
