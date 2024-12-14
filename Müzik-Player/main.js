f77285b07fbe1715eafbc5ac5cd0776fe37ffe24
Çınar Özgürgil <cozgurgil@gmail.com>
2024-12-14 15:30:25 +0300

    Yenilikler

--- Müzik-Player/main.js
+++ Müzik-Player/main.js
@@ -3,16 +3,30 @@ const img_music = document.querySelector("#img");
 const next = document.querySelector("#next");
 const resume_stop = document.querySelector("#resume-stop");
 const before = document.querySelector("#before");
+var TimeStart = document.querySelector("#timestart");
+var TimeEnd = document.querySelector("#timeend");
+
 
 const player = new MusicPlayer(musicList);
 
-function musicHead() {
-    music_head.innerHTML = player.musicList[0];
+window.addEventListener("load", () => { // Burada window sayfa anlamındadır yani aslında burada sayda load olduğu zaman yani yüklendiği zaman buradakileri çalıştır anlamındadır.
+    let music = player.getMusic();
+    displayMusic(music);
+});
+
+function displayMusic(music) {
+    music_head.innerText = music.getName(); 
+    img_music.src = "img/" + music.img;
+    audio.src = "mp3/" + music.file;
 }
 
-musicHead();
+resume_stop.addEventListener("click", () => {
+    let play = player.getMusic();
+    console.log(play);
+})
 
-next.addEventListener("click", () => {
-    
-});
+
+
+
+
 
